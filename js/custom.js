3a3edc02bd9840d5606086a32c392f493e065afb
Darren Cao <darrencaoyz@gmail.com>
2016-08-24 01:28:11 -0700

    add moving bg, remove plugins

--- js/custom.js
+++ js/custom.js
@@ -1,38 +1,15 @@
-function display() {
-  $('#navbar').addClass('animated fadeInDown');
-  $('#navbar').removeClass('hidden');
-  $('#links').addClass('animated fadeInUp');
-  $('#links').removeClass('hidden');
-  $('#links').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-    $('#particles').particleground({
-    dotColor: '#337ab7',
-    lineColor: '#AEC6CF',
-    particleRadius: 4,
-    density: 15000,
-    proximity: 100,
-    parallaxMultiplier: 25
-    });
-  });
-}
-
-$.typer.options = {
-  highlightSpeed    : 18,
-  typeSpeed         : 25,
-  clearDelay        : 500,
-  typeDelay         : 200,
-  clearOnHighlight  : true,
-  typerDataAttr     : 'data-typer-targets',
-  typerInterval     : 2000,
-  callback          : display
-}
-
 $('#name').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
   $('#desc').addClass('animated fadeIn');
   $('#desc').removeClass('hidden');
 });
 
 $('#desc').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-  $('#desc').typeTo('creative problem-solver / software engineer')
+  $('#navbar').addClass('animated fadeInDown');
+  $('#navbar').removeClass('hidden');
+  $('#links').addClass('animated fadeInUp');
+  $('#links').removeClass('hidden');
+  $('#animate-container').addClass('animated fadeIn');
+  $('#animate-container').removeClass('hidden');
 });
 
 $("#home-ln").click(function(e) {
